0a7a6076e82ed6da9dd010752cdc8542693a547a
cyub <qietingfy@gmail.com>
2017-09-20 20:26:34 +0800

    and an layer

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
-
-$starter = new Tink\Common\Starter(__DIR__ . '/../'); // the starter for manage slim application and boot something which the application need
+// the starter for manage slim application and boot something which the application need
+$starter = new Tink\Common\Starter(__DIR__ . '/../'); 
 $settings = require __DIR__ . '/../src/settings.php';
-$starter->setApplication($app = new \Slim\App($settings))
+$starter->setApplication($app = new Slim\App($settings))
 	->run();
